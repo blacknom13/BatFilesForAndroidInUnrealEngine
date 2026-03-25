73d51dceac90d3436c9dd221366809d1cbf5af81
Blacknom <67366744+blacknom13@users.noreply.github.com>
2026-03-25 09:42:40 +0100

    Update AndroidReconfig4.bat
    Update

--- AndroidReconfig4.bat
+++ AndroidReconfig4.bat
@@ -6,8 +6,8 @@ echo Updating Android SDK settings...
 
 (
 echo [/Script/AndroidPlatformEditor.AndroidSDKSettings]
-echo SDKPath^=(Path^="C:/Users/bumag/AppData/Local/Android/Sdk"^)
-echo NDKPath^=(Path^="C:/Users/bumag/AppData/Local/Android/Sdk/ndk/21.4.7075529"^)
+echo SDKPath^=(Path^="C:/Users/[user]/AppData/Local/Android/Sdk"^)
+echo NDKPath^=(Path^="C:/Users/[user]/AppData/Local/Android/Sdk/ndk/21.4.7075529"^)
 echo JavaPath^=(Path^="C:/Program Files/Android/Android Studio4.0/jre"^)
 echo SDKAPILevel=android-30
 echo NDKAPILevel=android-21
